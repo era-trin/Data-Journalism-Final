e5a8b301966e9666a02e6b3bac28a6c9fe389bc0
era-trin <ellan.amarasinghe26@trinityschoolnyc.org>
2025-06-01 15:18:47 -0400

    color application on heatmap refined

--- FinalDJProject/server.py
+++ FinalDJProject/server.py
@@ -9,7 +9,7 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 def hospitalized_to_hsl(life):
  
     legend = [
-        (50, "hsl(0, 100%, 70%)"),
+        (50, "hsl(0, 100%, 85%)"),
         (100, "hsl(0, 100%, 55%)"),
         (200, "hsl(0, 100%, 40%)"),
         (300, "hsl(0, 100%, 25%)"),
@@ -21,7 +21,7 @@ def hospitalized_to_hsl(life):
     for val, hsl in reversed(legend):
         if life >= val:
             return hsl
-    return "hsl(0,100%,5%)"
+    return "hsl(0,100%,85%)"
 
 
 @app.route('/about')
@@ -31,34 +31,30 @@ def home():
 
 @app.route('/')
 def macro_page():
-    selected_date = request.args.get('date', '02/29/2020')
     with open("FinalDJProject/Data/borough_data.json","r") as f:
         raw_data = json.load(f)
 
-    selected_data = None
-    for entry in raw_data:
-        if entry["Date"] == selected_date:
-            selected_data = entry
-    
     all_dates = [entry["Date"] for entry in raw_data]
+    selected_date = request.args.get('date')
+    if not selected_date or selected_date not in all_dates:
+        selected_date = all_dates[0]  # Use the first date in your data
+
+    selected_data = next((entry for entry in raw_data if entry["Date"] == selected_date), None)
 
     borough_colors = {
-    "manhattan":hospitalized_to_hsl(int(selected_data["MN_HOSPITALIZED_COUNT"])), 
-    "brooklyn":hospitalized_to_hsl(int(selected_data["BK_HOSPITALIZED_COUNT"])),
-    "statenisland":hospitalized_to_hsl(int(selected_data["SI_HOSPITALIZED_COUNT"])),
-    "queens":hospitalized_to_hsl(int(selected_data["QN_HOSPITALIZED_COUNT"])),
-    "bronx":hospitalized_to_hsl(int(selected_data["BX_HOSPITALIZED_COUNT"]))
+        "manhattan": hospitalized_to_hsl(int(selected_data["MN_HOSPITALIZED_COUNT"])), 
+        "brooklyn": hospitalized_to_hsl(int(selected_data["BK_HOSPITALIZED_COUNT"])),
+        "statenisland": hospitalized_to_hsl(int(selected_data["SI_HOSPITALIZED_COUNT"])),
+        "queens": hospitalized_to_hsl(int(selected_data["QN_HOSPITALIZED_COUNT"])),
+        "bronx": hospitalized_to_hsl(int(selected_data["BX_HOSPITALIZED_COUNT"]))
     }
    
-    
-    return render_template("index.html",dates=all_dates, borough_colors=borough_colors)
-
-    
-
-
-
-
-
+    return render_template(
+        "index.html",
+        dates=all_dates,
+        borough_colors=borough_colors,
+        selected_date=selected_date
+    )
 
 @app.route('/borough')
 def micro_page():
