3f2a3fb3b3d46b1db928573ae4f03f2d01f50364
era-trin <ellan.amarasinghe26@trinityschoolnyc.org>
2025-06-04 10:29:53 -0400

    Update server.py

--- FinalDJProject/server.py
+++ FinalDJProject/server.py
@@ -34,7 +34,7 @@ def macro_page():
     all_dates = [entry["Date"] for entry in raw_data]
     selected_date = request.args.get('date')
     if not selected_date or selected_date not in all_dates:
-        selected_date = all_dates[0]  # Use the first date in your data
+        selected_date = all_dates[0]  
 
     selected_data = next((entry for entry in raw_data if entry["Date"] == selected_date), None)
 
